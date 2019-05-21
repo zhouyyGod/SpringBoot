eca2de1357a55bd2004437174e7c304913b27997
Fear-Of-Loneliness <17727491320@163.com>
2019-05-21 17:26:24 +0800

    s

---
+++ demo/src/main/java/com/zyy/demo/DemoApplicatione.java
@@ -0,0 +1,18 @@
+package com.zyy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Create by ZYY on 2019/5/21 15:55
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class DemoApplicatione {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplicatione.class,args);
+    }
+
+}
